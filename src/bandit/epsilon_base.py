770d83122b64f3aca2466509573740c9a0d3e58a
Philipp Seitz <philipp.seitz@tum.de>
2020-04-06 16:08:09 +0200

    refactor and add ucb

--- src/bandit/epsilon_base.py
+++ src/bandit/epsilon_base.py
@@ -1,13 +1,20 @@
-import numpy as np
+import math
+
 import matplotlib.pyplot as plt
+import numpy as np
 
 from bandit import Bandit
 
 
-def run_experiment(number_bandits, iterations, epsilon=0):
-    optimistic = 10 if epsilon else 0
-    bandits = [Bandit(i + 1, optimistic) for i in range(number_bandits)]
-    current_best = bandits[np.random.randint(0, number_bandits)]
+def ucb(mean, n, n_j):
+    if n_j:
+        return mean + math.sqrt(2 * math.log(n) / n_j)
+    return float('inf')
+
+
+def run_greedy(number_bandits, epsilon, iterations):
+    bandits = [Bandit(i + 1, 0) for i in range(number_bandits)]
+    current_best = bandits[0]
     data = np.empty(iterations)
     print(f'Starting with bandit {current_best.true_mean}.')
     for i in range(iterations):
@@ -37,18 +44,62 @@ def run_experiment(number_bandits, iterations, epsilon=0):
     return cumulative_average
 
 
-if __name__ == "__main__":
-    epsilon_1 = run_experiment(3, 100000, 0.1)
-    epsilon_05 = run_experiment(3, 100000, 0.05)
-    epsilon_01 = run_experiment(3, 100000, 0.01)
+def run_optimistic(number_bandits, iterations):
+    bandits = [Bandit(i + 1, 10) for i in range(number_bandits)]
+    data = np.empty(iterations)
+    for i in range(iterations):
+        bandit = bandits[np.argmax([bandit.current_mean
+                                    for bandit in bandits])]
+        # exploit
+        value = bandit.pull()
+        bandit.update(value)
+        data[i] = value
 
-    optimistic = run_experiment(3, 100000)
+    cumulative_average = np.cumsum(data) / (np.arange(iterations) + 1)
+    plt.plot(cumulative_average)
+    for i in range(number_bandits):
+        plt.plot(np.ones(iterations) * (i + 1))
+    plt.xscale('log')
+    plt.show()
+    return cumulative_average
+
+
+def run_ucb(number_bandits, iterations):
+    bandits = [Bandit(i + 1, 10) for i in range(number_bandits)]
+    data = np.empty(iterations)
+    for i in range(iterations):
+        bandit = bandits[np.argmax([
+            ucb(bandit.current_mean, i, bandit.times_pulled)
+            for bandit in bandits
+        ])]
+        # exploit
+        value = bandit.pull()
+        bandit.update(value)
+        data[i] = value
+
+    cumulative_average = np.cumsum(data) / (np.arange(iterations) + 1)
+    plt.plot(cumulative_average)
+    for i in range(number_bandits):
+        plt.plot(np.ones(iterations) * (i + 1))
+    plt.xscale('log')
+    plt.show()
+    return cumulative_average
+
+
+if __name__ == "__main__":
+    epsilon_1 = run_greedy(3, 0.1, 100000)
+    epsilon_05 = run_greedy(3, 0.05, 100000)
+    epsilon_01 = run_greedy(3, 0.01, 100000)
+
+    optimistic = run_optimistic(3, 100000)
+    ucb = run_ucb(3, 100000)
 
     # log scale plot
     plt.plot(epsilon_1, label='eps = 0.1')
     plt.plot(epsilon_05, label='eps = 0.05')
     plt.plot(epsilon_01, label='eps = 0.01')
     plt.plot(optimistic, label='optimistic')
+    plt.plot(ucb, label='ucb')
     plt.legend()
     plt.xscale('log')
     plt.show()
